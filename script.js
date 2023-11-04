08350a3916b09760e49dba9b6c0de89432ae408a
weblinxsolution <103132259+weblinxsolution@users.noreply.github.com>
2023-11-04 19:27:45 +0500

    commit

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ function openNav() {
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+     let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+       current += increment;
+       obj.textContent = current;
+       if (current == end) {
+        clearInterval(timer);
+       }
+      }, step);
+    }
+    counter("count1", 0, 100, 3000);
+    counter("count2", 100, 20, 2500);
+    counter("count3", 0, 26, 3000);
+   });
+   
